0b20a8aa51d1bfb45d09a4dfdd93d33a9763b2e0
David Talan <david.talan2@mail.dcu.ie>
2020-12-10 15:25:51 +0000

    adding dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import GrainIcon from '@material-ui/icons/Grain';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
 }
+const useStyles = makeStyles((theme) => ({
+  link: {
+    display: 'flex',
+  },
+  icon: {
+    marginRight: theme.spacing(0.5),
+    width: 20,
+    height: 20,
+  },
+}));
 
-export default App;
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+export default function IconBreadcrumbs() {
+  const classes = useStyles();
+
+  return (
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link color="inherit" href="/" onClick={handleClick} className={classes.link}>
+        <HomeIcon className={classes.icon} />
+        Home
+      </Link>
+      <Link
+        color="inherit"
+        href="/getting-started/installation/"
+        onClick={handleClick}
+        className={classes.link}
+      >
+        <WhatshotIcon className={classes.icon} />
+        Core
+      </Link>
+      <Typography color="textPrimary" className={classes.link}>
+        <GrainIcon className={classes.icon} />
+        Breadcrumb
+      </Typography>
+    </Breadcrumbs>
+  );
+}
